92f102bf90a4adf1853dc1d5bc96bd44c83683a0
Lara Govand <laaara@live.se>
2025-03-17 16:07:39 +0100

    Lagt till sökruta i search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,32 @@
 
+import { useState } from 'react';
+
 const Search = () => {
+
+    const [city, setCity] = useState ('');
+
+    const handleInputChange = (event) => {
+        setCity(event.target.value);
+    };
 
     return (
         <div>
             <h2>Sök efter en stad</h2>
  {/* Här kommer vi senare lägga input-fält och knapp */}
-        </div>
+ <input
+ type="text"
+ value={city} //Binder input-fältet till vårt state
+ onChange={handleInputChange} // När användaren skriver uppdateras state placeholder
+     placeholder="Skriv in stadens namn"
+     />
+           {/* Här kan vi senare lägga till en knapp för att trigga sökningen */}
+  
+ </div>
     );
 
 
 
 };
+
 
 export default Search;
